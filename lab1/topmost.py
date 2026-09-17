a1e38498dffe1be44f241219e47b972832c645c6
xIpsie <82333583+xIpsie@users.noreply.github.com>
2026-09-17 11:07:52 +0200

    Real initial commit

---
+++ lab1/topmost.py
@@ -0,0 +1,4 @@
+# Input 1: En dictionary med ord och dess frekvenser
+# Input 2: Hur många av de vanligaste orden som ska printas
+def printTopMost(frequencies, n):
+    pass
---
+++ lab1/wordfreq.py
@@ -0,0 +1,14 @@
+# Input: En lista med text från docs
+# Output: Samma lista som har "tokenizats" dvs delats upp i tokens
+def tokenize(lines):
+    pass
+    # with open('examples/article1.txt', 'r', encoding='utf-8') as f:
+    #     content = f.read()
+    #     print(content)
+
+
+# Input 1: En lista med ord som ska räknas
+# Input 2: En lista med ointressanta ord som ska ignoreras
+# Output: En dictionary där orden är nycklar och värdena är frekvensen av ordet
+def countWords(words, stopwords):
+    pass
