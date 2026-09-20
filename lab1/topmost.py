f8c36fabdf0ab366c62c223c10fb1f38a0cf7c57
noah <xipsie@gmail.com>
2026-09-21 01:22:55 +0200

    Gjorde klart main funktionen, hantering av rekursiv funktion behöver revideras

--- lab1/topmost.py
+++ lab1/topmost.py
@@ -1,3 +1,9 @@
+import sys
+# Hårdkodad limit på hur många loops en rekursiv func kan köras för annars cappar på den 999 försök
+sys.setrecursionlimit(10000)
+import urllib.request
+from wordfreq import *
+
 # Input 1: Dictionary som sorteras baserat värdet. Störst till minst.
 # Return: Sorterad dictionary. 
 def sort(dict):
@@ -25,3 +31,30 @@ def printTopMost(frequencies, n):
 
     for i in range(n):
       print(word_list[i].ljust(10), frequencies[word_list[i]])
+
+
+# Input: Inputten anges i command prompten t.ex. "py topmost.py eng_stopwords.txt examples/article1.txt 20"
+# Där topmost.py --> sys.argv[0], eng_stopwords.txt --> sys.argv[1], examples/article1.txt --> sys.argv[2], 20 --> sys.argv[3]
+
+# Output: printTopMost() printar ut de n mest frekventa ord och deras statistik
+def main():
+  # Checkar om input-texten är en hemsida eller en .txt
+  if sys.argv[2][:7] == "http://" or sys.argv[2][:8] == "https://":
+    response = urllib.request.urlopen(sys.argv[2])  
+    raw_text = response.read().decode("utf8")
+  else:
+    with open(sys.argv[2], encoding="utf-8") as document:
+      raw_text = document.read()
+
+  stopwords_file, n = sys.argv[1], int(sys.argv[3]) 
+  text = tokenize(raw_text.split("\n"))
+    
+  with open(stopwords_file, encoding="utf-8") as document:
+    frequencies = countWords(text, document.read())
+
+  printTopMost(frequencies, n)
+
+
+
+if __name__ == "__main__":
+    main()
